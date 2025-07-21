d33f7efb435328118f58617adc888f9791fb2dda
nathansh1 <118627345+nathansh1@users.noreply.github.com>
2025-07-20 20:01:26 -0500

    feat: better sign in screen
    - designed sign in screen
    - improved error handling during sign-in
    - new app icon
    - enforce dark mode across app
    - add new dependencies

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,12 +1,10 @@
-import { router } from 'expo-router'
-import { useAuth } from '@/components/auth/auth-provider'
-import { AppText } from '@/components/app-text'
 import { AppView } from '@/components/app-view'
-import { AppConfig } from '@/constants/app-config'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { ActivityIndicator, View } from 'react-native'
-import { Image } from 'expo-image'
+import { useAuth } from '@/components/auth/auth-provider'
 import { Button } from '@react-navigation/elements'
+import { Image } from 'expo-image'
+import { router } from 'expo-router'
+import { ActivityIndicator, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function SignIn() {
   const { signIn, isLoading } = useAuth()
@@ -24,28 +22,30 @@ export default function SignIn() {
         <SafeAreaView
           style={{
             flex: 1,
-            justifyContent: 'space-between',
           }}
         >
-          {/* Dummy view to push the next view to the center. */}
-          <View />
-          <View style={{ alignItems: 'center', gap: 16 }}>
-            <AppText variant="headlineMedium">{AppConfig.name}</AppText>
-            <Image source={require('../assets/images/icon.png')} style={{ width: 128, height: 128 }} />
-          </View>
-          <View style={{ marginBottom: 16 }}>
-            <Button
-              variant="filled"
-              style={{ marginHorizontal: 16 }}
-              onPress={async () => {
-                await signIn()
-                // Navigate after signing in. You may want to tweak this to ensure sign-in is
-                // successful before navigating.
-                router.replace('/')
-              }}
-            >
-              Connect
-            </Button>
+          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <View style={{ alignItems: 'center', gap: 100 }}>
+              <Image source={require('../assets/images/flick-logo-no-bg.png')} style={{ width: 378, height: 130 }} />
+              <Button
+                variant="filled"
+                style={{ marginHorizontal: 16 }}
+                onPress={async () => {
+                  try {
+                    console.log('Signing in...')
+                    const account = await signIn();
+                    console.log('Sign in successful');
+                    // Navigate after signing in
+                    router.replace('/');
+                  } catch (error) {
+                    console.log(`Sign in failed: ${error instanceof Error ? error.message : 'Unknown error'}`);
+                    alert(`Sign in failed: ${error instanceof Error ? error.message : 'Unknown error'}`);
+                  }
+                }}
+              >
+                Connect
+              </Button>
+            </View>
           </View>
         </SafeAreaView>
       )}
--- constants/app-config.tsx
+++ constants/app-config.tsx
@@ -1,10 +1,10 @@
-import { clusterApiUrl } from '@solana/web3.js'
 import { Cluster } from '@/components/cluster/cluster'
 import { ClusterNetwork } from '@/components/cluster/cluster-network'
+import { clusterApiUrl } from '@solana/web3.js'
 
 export class AppConfig {
   static name = 'Flick'
-  static uri = 'https://example.com'
+  static uri = 'https://flick.app'
   static clusters: Cluster[] = [
     {
       id: 'solana:devnet',
